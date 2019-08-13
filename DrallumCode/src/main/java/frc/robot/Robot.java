f6bb98cd2565b0b92f2a3174735e819a141621f7
jamiha <34746242+jamiha@users.noreply.github.com>
2019-08-13 19:58:17 -0400

    This is base driving code
    This is the drive code for Drallam

---
+++ DrallumCode/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,67 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Robot extends TimedRobot {
+
+  private DifferentialDrive myDrive;
+
+  private Spark Left, Right;
+
+  private XboxController Controller;
+
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  public void robotInit() {
+
+    Left = new Spark(0);
+    Right = new Spark(1);
+
+    myDrive = new DifferentialDrive(Left, Right);
+
+    Controller = new XboxController(0);
+
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  public void robotPeriodic() {
+  }
+
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  public void teleopPeriodic() {
+    myDrive.arcadeDrive(Controller.getRawAxis(1) *0.7, Controller.getRawAxis(0)*0.7);
+
+
+  }
+
+  public void testPeriodic() {
+  }
+}
